c5c2fbd953dc512a69c3e3b6e70985ece76c77bc
katie-truong <khntruong@ucdavis.edu>
2018-06-22 15:16:02 -0700

    Implement fetch()

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,38 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+  this.fetch();
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: this.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data.results);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+//   endpoint: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+//   init: function(){},
+//   send: function(){},
+//   fetch: function(){},
+//   clearMessages: function(){},
+//   renderMessage: function(){},
+//   addRoom: function(){},
+//   renderRoom: function(){},
+//   handleUsernameClick: function(){},
+//   handleSubmit: function(){},
+//   messages: []
+
+$(document).ready(function() {
+  app.init();
+});
+
